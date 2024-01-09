2f665862a0390f30cfca035952fab5ccdc115d81
TamaraHajjar <135793631+TamaraHajjar@users.noreply.github.com>
2024-01-09 19:01:46 +0100

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.pond_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.pond_project to javafx.fxml;
-    exports com.example.pond_project;
-}
